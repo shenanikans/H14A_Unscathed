37f5a8b42e6e627d80f14b9a99f285d2f09de017
Danielle Wu <z5689177@ad.unsw.edu.au>
2026-03-12 22:29:57 +1100

    Wrote get_despatch_advice_by_id function and updated lambda_function.py to handle the http request

---
+++ src/retrieve_despatch_by_id.py
@@ -0,0 +1,38 @@
+# Import required modules for the API
+import json
+import boto3
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+
+# Import helper function and constants to build the JSON response
+from helper_functions import build_response
+from constants import JSON_TYPE, XML_TYPE
+
+# Initialise DynamoDB table
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb_table = dynamodb.Table('despatch_advices')
+
+def get_despatch_advice_by_id(despatch_id):
+    """ Retrieves the despatch advice with the corresponding despatch ID if the ID provided is valid.
+
+    Args:
+        despatch_id: int that indicates the corresponding ID of the despatch advice document to be retrieved
+    
+    Returns: 
+        Response: JSON object structure detailing the statusCode, Content-Type, and body
+    """
+
+    try:
+        # Try to retrieve the despatch advice using despatch_id
+        response = dynamodb_table.get_item(Key={'despatch_id': despatch_id})
+
+        # Return error if despatch advice does not exist
+        if 'Item' not in response:
+            return build_response(404, JSON_TYPE, f'Despatch advice {despatch_id} not found')
+
+        # Else return the despatch advice document
+        return build_response(200, XML_TYPE, response['Item']['document'])
+
+    except ClientError as e:
+        print('Error:', e)
+        return build_response(400, JSON_TYPE, e.response['Error']['Message'])
