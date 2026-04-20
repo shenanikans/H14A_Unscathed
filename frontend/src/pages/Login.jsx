e84bf19d7c1141ec83ad8f36bad3eb212936c324
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-21 01:26:53 +1000

    Login Page updated with background and colours and logo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
     const handleSubmit = async (event) => {
         event.preventDefault()
 
@@ -16,16 +17,27 @@ export default function Login() {
         console.log(email, password)
         }
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" value={email}
-                    onChange={(event) => setEmail(event.target.value)}/>
-                <input type="password" placeholder="Password" value={password}
-                    onChange={(event) => setPassword(event.target.value)}/>
-                <button type="submit">Log in</button>
-            </form>
-            <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+        <div 
+            className="min-h-screen flex items-center justify-center"
+            style={{ 
+                backgroundImage: "url('/babyblue_background.jpg')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+            }}
+        >
+            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
+                <h1 className="font-conthrax text-6xl mb-5 text-center">Atlas</h1>
+                <p className="text-gray-500 mb-4 text-center">Sign in to your account</p>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4" placeholder="Email" value={email}
+                        onChange={(event) => setEmail(event.target.value)}/>
+                    <input type="password" className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4" placeholder="Password" value={password}
+                        onChange={(event) => setPassword(event.target.value)}/>
+                    <button type="submit" className="w-full bg-deep-sky-blue-600 text-white py-2 rounded-lg hover:bg-deep-sky-blue-700">Log In</button>
+                </form>
+                <p className="text-sm text-gray-500 mt-4 text-center">Don't have an account? <Link to="/register" className="text-deep-sky-blue-600 hover:underline">Sign up</Link></p>
+            </div>
+            
         </div>
     )
 }
